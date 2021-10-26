47c9f0253aad043e10f4b8ba8fe66d358432fbc2
Ognjen Vulic <ogi@goowall.io>
2021-10-26 23:26:38 +0200

    Emotion CSS working.

--- src/routes/Home/components/BoardBadge.jsx
+++ src/routes/Home/components/BoardBadge.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+/** @jsxImportSource @emotion/react */
 import { css, withTheme } from '@emotion/react';
 // import cx from 'classnames';
 // import commonClasses from 'styles/common/common.scss';
-const root = props => css`
-  height: 150px;
+const root = css`
+    padding: 32px;
+    background-color: hotpink;
+    font-size: 24px;
+    border-radius: 4px;
+    height: 150px;
 `;
 
 function BoardBadge(props) {
   const { board } = props;
   return (
-    <div css={root(props)}>
+    <div css={css`
+    
+  `}>
       <Link to={`/board/${board.id}`}>
         {board.name && <div>{board.name}</div>}
       </Link>
